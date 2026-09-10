85305af705f57aade3008e6514b2a82dfb0f48f7
anuhole <anushkahole18@gmail.com>
2026-09-10 16:40:13 +0530

    branch 1 updated

--- Hello.java
+++ Hello.java
@@ -1,9 +1,11 @@
 class Hello {
     public void greet(String name) {
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Good morning, " + name + "!");
     }
     public static void main(String[] args) {
+        // Edit the print line to say "Good  morning, yourname.
+        
         Hello hello = new Hello();
-        hello.greet("World");
+        hello.greet("Anushka");
     }
 }
