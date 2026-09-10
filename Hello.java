d21869936244201a4f55eb2a0310fb62939a6982
anuhole <anushkahole18@gmail.com>
2026-09-10 16:41:55 +0530

    main  updated

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 class Hello {
     public void greet(String name) {
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hey there, " + name + "!");
     }
     public static void main(String[] args) {
         Hello hello = new Hello();
-        hello.greet("World");
+        hello.greet("friends");
     }
 }
